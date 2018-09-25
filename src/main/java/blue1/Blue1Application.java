abd5228519c29de080b071bfca5c1c3279133bd2
Matt Cadorette <mcadorette@gmail.com>
2018-09-25 12:09:34 -0400

    Initial commit from Atomist

---
+++ src/main/java/blue1/Blue1Application.java
@@ -0,0 +1,12 @@
+package blue1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blue1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blue1Application.class, args);
+	}
+}
---
+++ src/test/java/blue1/Blue1ApplicationTests.java
@@ -0,0 +1,16 @@
+package blue1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Blue1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
